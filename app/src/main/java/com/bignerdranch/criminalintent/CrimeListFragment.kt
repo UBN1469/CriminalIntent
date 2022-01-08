04a5fe3f252fc9ca650def1ed3ec5e45c34cabc1
UBN100 <5423410@mail.ru>
2022-01-08 13:04:41 +0300

    231 стр
    Сделал нажатие на вьюхолдер

--- app/src/main/java/com/bignerdranch/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/criminalintent/CrimeListFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -55,10 +56,24 @@ class CrimeListFragment : Fragment(){
     }
     // Контейнер для представления
     private  inner class  CrimeHolder (view: View)
-        : RecyclerView.ViewHolder(view){
-            val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
-            val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        : RecyclerView.ViewHolder(view), View.OnClickListener{
+            private lateinit var crime: Crime
+            private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
+            private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        init {
+            itemView.setOnClickListener(this)
         }
+
+        fun dind (crime: Crime){
+            this.crime = crime
+            titleTextView.text =this.crime.title
+            dateTextView.text = this.crime.date.toString()
+        }
+
+        override fun onClick(v: View?) {
+            Toast.makeText(context,"${crime.title} pressed!", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     private inner class CrimeAdapter (var crimes: List<Crime>): RecyclerView.Adapter<CrimeHolder>(){
 
@@ -69,10 +84,11 @@ class CrimeListFragment : Fragment(){
 
         override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
            val crime = crimes[position]
-            holder.apply {
-                titleTextView.text = crime.title
-                dateTextView.text = crime.date.toString()
-            }
+//            holder.apply {
+//                titleTextView.text = crime.title
+//                dateTextView.text = crime.date.toString()
+//            }
+          holder.dind(crime)
         }
 
         override fun getItemCount(): Int {
